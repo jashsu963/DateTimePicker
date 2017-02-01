170338615ea948714a49c3cd8b8428ba3d870e2b
Nhan Dinh <jashsu963@gmail.com>
2017-02-01 18:19:58 +0700

    DateTimePicker

---
+++ app/src/main/java/com/sunrise/datetimepicker/MainActivity.java
@@ -0,0 +1,50 @@
+package com.sunrise.datetimepicker;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private DatePicker datePicker;
+    private TimePicker timePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        datePicker = (DatePicker) findViewById(R.id.dp_Date);
+        timePicker = (TimePicker) findViewById(R.id.tp_Time);
+        initDatePicker();
+        initTimepicker();
+    }
+
+
+    public void initDatePicker() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(calendar.YEAR);
+        int month = calendar.get(calendar.MONTH);
+        int day = calendar.get(calendar.DAY_OF_MONTH);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Toast.makeText(MainActivity.this, dayOfMonth + "-" + (monthOfYear+1) + "-" + year, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void initTimepicker(){
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                Toast.makeText(MainActivity.this, hourOfDay + ":" + minute, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
